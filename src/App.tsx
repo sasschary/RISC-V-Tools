d5a94f7a3412e199a7e44084f1c65471aa441f3f
Zach Clark <zclark@sassynetwork.com>
2020-11-04 19:08:02 -0500

    Moves disassembler into separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Button from 'react-bootstrap/Button';
 
 import './App.css';
 import './disassembler/disassembler';
-import disassembler from "./disassembler/disassembler";
+import Disassembler from "./disassembler/Disassembler.component";
 
 /************************* TYPES *************************/
 
 interface AppProps {}
 
-interface AppState {
-    instruction: string;
-    disassembled: string;
-}
+interface AppState {}
 
 
 /*********************** COMPONENT ***********************/
@@ -24,34 +17,8 @@ export default class App extends React.Component<AppProps, AppState> {
   render() {
       return (
           <div className="App">
-              <Form>
-                  <Form.Label>Assembled Instruction</Form.Label>
-                  <InputGroup>
-                      <InputGroup.Prepend>
-                          <InputGroup.Text>0x</InputGroup.Text>
-                      </InputGroup.Prepend>
-                      <FormControl onChange={(evt) => this.setState({instruction: evt.target.value})} />
-                      <InputGroup.Append>
-                          <Button onClick={this.handleDisassemble}>Disassemble!</Button>
-                      </InputGroup.Append>
-                  </InputGroup>
-                  <Form.Text>{this.state.disassembled}</Form.Text>
-              </Form>
+              <Disassembler />
           </div>
       );
-  }
-
-  constructor(props: {} | Readonly<{}>) {
-      super(props);
-      this.handleDisassemble = this.handleDisassemble.bind(this);
-      this.state = {
-          disassembled: '',
-          instruction: ''
-      }
-  }
-
-  handleDisassemble() {
-      const disassembled = disassembler.disassembleInstruction(this.state.instruction);
-      this.setState({disassembled});
   }
 }
---
+++ src/disassembler/Disassembler.component.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+
+import '../App.css';
+import '../disassembler/disassembler';
+import disassembler from "../disassembler/disassembler";
+
+/************************* TYPES *************************/
+
+interface DisassemblerProps {}
+
+interface DisassemblerState {
+    instruction: string;
+    disassembled: string;
+}
+
+
+/*********************** COMPONENT ***********************/
+
+export default class Disassembler extends React.Component<DisassemblerProps, DisassemblerState> {
+    render() {
+        return (
+            <div className="tool-container">
+                <Form>
+                    <Form.Label>Assembled Instruction</Form.Label>
+                    <InputGroup>
+                        <InputGroup.Prepend>
+                            <InputGroup.Text>0x</InputGroup.Text>
+                        </InputGroup.Prepend>
+                        <FormControl onChange={(evt) => this.setState({instruction: evt.target.value})} />
+                        <InputGroup.Append>
+                            <Button onClick={this.handleDisassemble}>Disassemble!</Button>
+                        </InputGroup.Append>
+                    </InputGroup>
+                    <Form.Text>{this.state.disassembled}</Form.Text>
+                </Form>
+            </div>
+        );
+    }
+
+    constructor(props: {} | Readonly<{}>) {
+        super(props);
+        this.handleDisassemble = this.handleDisassemble.bind(this);
+        this.state = {
+            disassembled: '',
+            instruction: ''
+        }
+    }
+
+    handleDisassemble() {
+        const disassembled = disassembler.disassembleInstruction(this.state.instruction);
+        this.setState({disassembled});
+    }
+}
